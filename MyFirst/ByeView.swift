1882ef91fdeb4604bfbee45cc9e252d5b66de497
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-01-25 20:12:10 +0900

    Initial Commit

---
+++ MyFirst/ByeView.swift
@@ -0,0 +1,27 @@
+//
+//  ByeView.swift
+//  MyFirst
+//
+//  Created by Brandon Jones on 1/25/24.
+//
+
+import SwiftUI
+
+
+struct ByeView: View {
+  
+  var body: some View {
+    Image(systemName: "globe")
+      .imageScale(.large)
+      .foregroundColor(.accentColor)
+    Text("Bye bye, World!")
+  }
+  
+  
+}
+
+
+#Preview {
+  
+  ByeView()
+}
